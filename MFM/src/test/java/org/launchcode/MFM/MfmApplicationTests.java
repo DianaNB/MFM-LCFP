46e3c8028d1e0891884398ab74745027cc6d54f1
Diana Buschman <diana.n.buschman@gmail.com>
2019-11-25 12:08:37 -0500

    Initial project commit

---
+++ MFM/src/test/java/org/launchcode/MFM/MfmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.MFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
